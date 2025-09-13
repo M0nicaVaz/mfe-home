bdc11b2b4bfcc5b2f9c684d58e41ceb78e65119e
Monica Vaz <monica.vaz@eumedicoresidente.com.br>
2025-09-13 11:17:29 -0300

    feat: mfe config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  assetPrefix: '/home',
+  basePath: '/home',
+  experimental: {
+    externalDir: true,
+  },
 };
 
 export default nextConfig;
