93bfd4889de4fcb99701643b456cbb7e439fe3f2
Monica Vaz <monica.vaz@eumedicoresidente.com.br>
2025-09-15 10:01:58 -0300

    chore: fix redirects

--- next.config.ts
+++ next.config.ts
@@ -22,15 +22,6 @@ const nextConfig: NextConfig = {
     };
     return config;
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: false,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
