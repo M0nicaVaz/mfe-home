fda4a1722599aecd715d4c1b07bf18dae8bb3143
Monica Vaz <monica.vaz@eumedicoresidente.com.br>
2025-09-22 21:07:51 -0300

    fix: hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
-import MenuResponsive from "@/components/MenuResponsive";
-import "../../../shared/styles/globals.scss";
-import styles from "./styles.module.scss";
-import { ReactNode } from "react";
+import MenuResponsive from '@/components/MenuResponsive';
+import '../../../shared/styles/globals.scss';
+import styles from './styles.module.scss';
+import { ReactNode } from 'react';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" suppressHydrationWarning>
       <head />
       <body>
         <div className={styles.container}>
           <MenuResponsive />
           {children}
         </div>
-        </body>
+      </body>
     </html>
   );
 }
