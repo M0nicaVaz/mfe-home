b9d873c978654dd26ba0469bb93d9faf407483ce
Monica Vaz <monica.vaz@eumedicoresidente.com.br>
2025-09-22 21:17:41 -0300

    lint: unused vars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,20 @@
-"use client";
+'use client';
 
-import { Transaction } from "@/models/Transaction";
-import BalanceCard from "../components/BalanceCard";
-import Header from "../components/Header";
-import MenuResponsive from "../components/MenuResponsive";
-import NewTransactionCard from "../components/NewTransactionCard";
-import StatementList from "../components/StatementList";
-import styles from "./styles.module.scss";
-import { useState } from "react";
-import { transactions as mockTransactions } from "@/data/index";
-import { useMediaQuery } from "react-responsive";
+import { Transaction } from '@/models/Transaction';
+import BalanceCard from '../components/BalanceCard';
+import Header from '../components/Header';
+import styles from './styles.module.scss';
+import { useState } from 'react';
+import { transactions as mockTransactions } from '@/data/index';
 
 export default function Home() {
-  const [transactions, setTransactions] =
-    useState<Transaction[]>(mockTransactions);
-
-  const isMobile = useMediaQuery({ maxWidth: 599 });
-
-  const handleAddTransaction = (tx: Transaction) => {
-    setTransactions((prev) => [tx, ...prev]);
-  };
-
-  const handleUpdateTransactions = (updated: Transaction) => {
-    setTransactions((prev) =>
-      prev.map((t) => (t.id === updated.id ? updated : t))
-    );
-  };
-
-  const handleDeleteTransaction = (deleted: Transaction) => {
-    setTransactions((prev) => prev.filter((t) => t.id !== deleted.id));
-  };
+  const [transactions] = useState<Transaction[]>(mockTransactions);
 
   return (
     <div className={styles.main}>
-      <div className={styles.layout}><Header /></div>
+      <div className={styles.layout}>
+        <Header />
+      </div>
       <div className={styles.content}>
         <div className={styles.left}>
           <BalanceCard transactions={transactions} />
